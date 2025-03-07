db6e01680c6c1d03281b67fda16270862f7ff0b7
Phyona Schrader <pschrad2@nd.edu>
2025-03-07 17:07:29 +0000

    First commit with vite-project

---
+++ vite-project/src/environment.js
@@ -0,0 +1,5 @@
+module.exports = {
+APPLICATION_ID: "VubAa2UZPA6Bivnevgnx5yCsYcICMEt8sEun5CPD",
+JAVASCRIPT_KEY: "LwRRby6u1It6xtY5ZBEwdgsWXem16c3ycUfTgFIS",
+SERVER_URL: "https://parseapi.back4app.com/"
+};
